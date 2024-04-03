83fc39801da77c5a15a68b894cb227322ed02a64
HaTruong05 <125408638+HaTruong05@users.noreply.github.com>
2024-04-03 01:20:43 -0400

    Creates a grid that tracks mouse movement

--- javascript.js
+++ javascript.js
@@ -6,9 +6,19 @@ function makeBoard(){
     for (let i = 0; i < ROW; i++){
         const row = document.createElement("div");
         row.classList.add("row");
+        if (i === COL - 1){
+            row.classList.add("bottom-edge")
+        } else if (i === 0){
+            row.classList.add("top-edge")
+        }
         for (let j = 0; j < COL; j++){
             const square = document.createElement("div");
             square.classList.add("square");
+            if (j === COL - 1){
+                square.classList.add("right-edge")
+            } else if (j === 0){
+                square.classList.add("left-edge")
+            }
             row.appendChild(square);
         }
         container.appendChild(row);
