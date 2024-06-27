58df4da1688e6a1ecaf1123240ba1f58d279d3bc
HaTruong05 <125408638+HaTruong05@users.noreply.github.com>
2024-06-27 16:33:16 -0400

    Allow the user to determine the grid size

--- javascript.js
+++ javascript.js
@@ -1,24 +1,28 @@
-const ROW = 16;
-const COL = 16;
+const GEN = document.querySelector('#genBoard');
+const MAX_PIX = 100;
+const MIN_PIX = 16;
 
-function makeBoard(){
+function makeBoard(n){
     const container = document.querySelector(".container")
-    for (let i = 0; i < ROW; i++){
+    while(container.firstChild){ // empty the board before making the new one
+        container.removeChild(container.firstChild);
+    }
+    for (let i = 0; i < n; i++){
         const row = document.createElement("div");
         row.classList.add("row");
-        if (i === COL - 1){
-            row.classList.add("bottom-edge")
+        if (i === n - 1){
+            row.classList.add("bottom-edge");
         } else if (i === 0){
-            row.classList.add("top-edge")
+            row.classList.add("top-edge");
         }
-        for (let j = 0; j < COL; j++){
+        for (let j = 0; j < n; j++){
             const square = document.createElement("div");
             square.classList.add("square");
-            square.addEventListener('mouseover', (e) => {e.target.classList.add("square-hover")})
-            if (j === COL - 1){
-                square.classList.add("right-edge")
+            square.addEventListener('mouseover', (e) => {e.target.classList.add("square-hover")});
+            if (j === n - 1){
+                square.classList.add("right-edge");
             } else if (j === 0){
-                square.classList.add("left-edge")
+                square.classList.add("left-edge");
             }
             row.appendChild(square);
         }
@@ -26,4 +30,34 @@ function makeBoard(){
     }
 }
 
-makeBoard()
+makeBoard(MIN_PIX)
+
+function newBoardFromInput(){
+    n = getInput();
+    makeBoard(n);
+}
+
+function getInput(){
+    let input = prompt('Please choose a value between 16-100 to generate a new board. ' + 
+                         'A higher value will result in a more detailed board. ' + 
+                         'A lower value will result in a more pixelated board.');
+    while (true){
+        if (isNumeric(input) && MIN_PIX <= input && input <= MAX_PIX){
+            return parseInt(input);
+        } else {
+            input = prompt("You didn't input a number or the number is too large or too small. " + 
+                          'Please choose a value between 16-100 to generate a new board.');
+        }
+    }
+}
+
+function isNumeric(str) {
+    if (typeof str != "string") return false;
+    return !isNaN(str) && 
+           !isNaN(parseFloat(str));
+  }
+
+GEN.addEventListener('click', newBoardFromInput);
+
+
+
